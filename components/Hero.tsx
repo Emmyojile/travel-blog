0f47d99d10b91e8e513b37b640f4963348f4aee9
Emmyojile <emmaojile99@gmail.com>
2023-12-25 04:12:52 -1100

    hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
 
        {/* LEFT */}
 
-       <div className="relative">
+       <div className="relative z-20 flex flex-1 flex-col xl:w:1/2">
         <Image
         src="/camp.svg"
         alt='camp'
@@ -16,6 +16,25 @@ const Hero = () => {
         height={50}
         className='absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]'
         />
+        <h1 className='bold-52 lg:bold-88'>Legacy Hills Open Camp</h1>
+        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos iste tenetur explicabo quae esse magni?Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur fugiat sunt qui!</p>
+        <div className="my-11 flex flex-wrap gap-5">
+          <div className="flex items-center gap-2">
+            {Array(5).fill(1).map((_, index) => (
+              <Image
+              src="/star.svg"
+              key={index}
+              alt='star'
+              width={24}
+              height={24}
+              />
+            ))}
+          </div>
+          <p className="bold-16 lg:bold-20 text-blue-70">
+            189k
+            <span className="regular-16 lg:regular-20 ml-1">Excellent Reviews</span>
+          </p>
+        </div>
        </div>
     </section>
   )
