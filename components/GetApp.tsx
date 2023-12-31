d50cf61d4c5204b35158516255ea868d388f5bd7
Emmyojile <emmaojile99@gmail.com>
2024-01-01 00:43:16 +0100

    get app section

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import Button from './Button'
-import Image from 'next/image'
+import React from "react";
+import Button from "./Button";
+import Image from "next/image";
 
 const GetApp = () => {
   return (
-    <section className='flexCenter w-full flex-col pb-[100px]'>
+    <section className="flexCenter w-full flex-col pb-[100px]">
       <div className="get-app">
         {/* LEFT */}
         <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
@@ -14,37 +14,31 @@ const GetApp = () => {
           <p className="regular-16 text-gray-10">
             Available on ios and Android
           </p>
-          <div className='flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row'>
+          <div className="flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row">
             <Button
-            type='button'
-            title='App Store'
-            icon='/apple.svg'
-            variant='btn_white'
-            full
+              type="button"
+              title="App Store"
+              icon="/apple.svg"
+              variant="btn_white"
+              full
             />
             <Button
-            type='button'
-            title='Play Store'
-            icon='/android.svg'
-            variant='btn_dark_green_outline'
-            full
+              type="button"
+              title="Play Store"
+              icon="/android.svg"
+              variant="btn_dark_green_outline"
+              full
             />
           </div>
         </div>
 
         {/* RIGHT */}
         <div className="flex flex-1 items-center justify-end">
-        <Image
-        src='/phones.png'
-        alt='phones'
-        width={550}
-        height={870}
-        />
-
+          <Image src="/phones.png" alt="phones" width={550} height={870} />
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default GetApp
+export default GetApp;
